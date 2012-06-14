c5f4ffcaf8700ffad67e31422dec10be0de56d1c
melot.philippe <melot.philippe@282cbf28-0b21-c27b-f752-b18d30e2b6b0>
2012-06-14 13:07:13 +0000

    tagged 2.1.10

---
+++ tags/2.1.10/libraries/ManiaLiveApplication/Application.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * ManiaLive - TrackMania dedicated server manager in PHP
+ * 
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+
+namespace ManiaLiveApplication;
+
+use ManiaLive\Features\Updater;
+
+const Version = '2.1.10';
+
+class Application extends \ManiaLive\Application\AbstractApplication
+{
+	protected function init()
+	{
+		parent::init();
+		
+		if(!\ManiaLive\Config\Config::getInstance()->lanMode)
+			Updater::getInstance();
+	}
+}
+
+?>
